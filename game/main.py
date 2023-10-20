8501ef78eaf2f62b47b910dfadbd27b9e09600f3
nils-nm <nilsfuruskog@yandex.ru>
2023-10-20 14:59:18 +0300

    feet: collisible item & scene & smoothly move of camera
    in the futere add a normal sprite for ammo

--- game/main.py
+++ game/main.py
@@ -8,6 +8,7 @@ sc_title = 'test'
 #const for scale
 charecter_scal = 1
 tile_scal = 0.5
+ammo_scal = 0.3
 
 #movment speed per frame
 player_sp = 5
@@ -27,6 +28,15 @@ class MyGame(ar.Window):
         #separete variable that holds the playre sprite
         self.player_sprite = None
 
+        # A Camera that can be used for scrolling the screen
+        self.camera = None
+
+        #gui camera
+        self.gui_camera = None
+
+        #keep trak score
+        self.bullets = 0
+
         #set bg color
         ar.set_background_color(ar.color.ARSENIC)
 
@@ -34,6 +44,15 @@ class MyGame(ar.Window):
     def setup(self):
 
         #setup the game
+
+        # Set up the Camera
+        self.camera = ar.Camera(self.width, self.height)
+
+        #set up gui camera
+        self.gui_camera = ar.Camera(self.width, self.height)
+
+        #keep track score
+        self.bullets = 10
 
         #init scene
         self.scene = ar.Scene()
@@ -56,6 +75,14 @@ class MyGame(ar.Window):
             wall.position = cord
             self.scene.add_sprite('Walls', wall)
 
+
+        # use loop for create some ammo
+        for x in range(128, 1250, 256):
+            bullet = ar.Sprite('images/coinBronze.png', ammo_scal)
+            bullet.center_x = x
+            bullet.center_y = 256
+            self.scene.add_sprite('Ammo', bullet)
+
         #create physics engine
         self.physics_engine = ar.PhysicsEngineSimple(
                 self.player_sprite, self.scene.get_sprite_list('Walls')
@@ -70,39 +97,77 @@ class MyGame(ar.Window):
         #draw our scene
         self.scene.draw()
 
+        #activate gui camera
+        self.gui_camera.use()
+
+        #draw our ammo on the screen
+        ammo_text = f'ammo: {self.bullets}'
+        ar.draw_text(
+                ammo_text,
+                10, 10,
+                ar.color.WHITE,
+                18)
+
 
     def on_key_press(self, key, modifiers):
 
         if key == ar.key.W:
             self.player_sprite.change_y = player_sp
-        elif key == ar.key.S:
+        if key == ar.key.S:
             self.player_sprite.change_y = -player_sp
-        elif key == ar.key.A:
+        if key == ar.key.A:
             self.player_sprite.change_x = -player_sp
-        elif key == ar.key.D:
+        if key == ar.key.D:
             self.player_sprite.change_x = player_sp
 
 
 
     def on_key_release(self, key, modifiers):
 
-        if key == ar.key.W:
+
+        if key == ar.key.W or key == ar.key.S:
             self.player_sprite.change_y = 0
-        elif key == ar.key.S:
-            self.player_sprite.change_y = 0
-        elif key == ar.key.A:
+        elif key == ar.key.A or key == ar.key.D:
             self.player_sprite.change_x = 0
-        elif key == ar.key.D:
-            self.player_sprite.change_x = 0
+
+    def center_camera_to_player(self):
+        screen_center_x = self.player_sprite.center_x - (self.camera.viewport_width / 2)
+        screen_center_y = self.player_sprite.center_y - (self.camera.viewport_height / 2)
+
+        #Don't let camera travel past 0
+        if screen_center_x < 0:
+            screen_center_x = 0
+        if screen_center_y < 0:
+            screen_center_y = 0
+        player_centered = screen_center_x, screen_center_y
+
+        #move camera to player with smoothly
+        self.camera.move_to(player_centered, 0.1)
 
 
     def on_update(self, delta_time):
 
         #game logic
 
+        # Activate our Camera
+        self.camera.use()
+
         #move player
         self.physics_engine.update()
 
+        #see if we hit any ammo
+        ammo_hit_list = ar.check_for_collision_with_list(
+                self.player_sprite, self.scene['Ammo'])
+
+        #loop throught each ammo we hit and remove it
+        for bullet in ammo_hit_list:
+            #remove ammo
+            bullet.remove_from_sprite_lists()
+            # add 10 bullet to ammo
+            self.bullets += 10
+
+        #Position the camera
+        self.center_camera_to_player()
 
 def main():
     window = MyGame()
